deeaf5795556a2f0b2108f928ded024443be6a64
Nudnick007 <nikhil_b210758ec@nitc.ac.in>
2024-07-09 19:48:14 +0530

    made more routings

---
+++ app/chart/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Chart() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <p className="text-sky-500 text-3xl mb-4">Hello World!</p>
+        <Link href="about" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+            Go to About Page
+        </Link>
+      </div>
+    );
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,49 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { FormEvent } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.currentTarget);
+    const name = formData.get('name');
+    const amount = formData.get('amount');
+
+    console.log({ name, amount });
+
+    router.push('/chart');
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <p className="text-sky-500 text-3xl mb-4">Hello World!</p>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          className="mb-2 p-2 border border-gray-300 rounded"
+          required
+        />
+
+        <input
+          type="number"
+          name="amount"
+          placeholder="Enter Amount"
+          className="mb-4 p-2 border border-gray-300 rounded"
+          required
+        />
+
+        <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 mb-4">
+          Submit
+        </button>
+      </form>
+
       <Link href="about" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
-          Go to About Page
+        Go to About Page
       </Link>
     </div>
   );
