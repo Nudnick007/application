f149655a9a69ad983fcc26f4cd3edc603b6b247b
Nudnick007 <nikhil_b210758ec@nitc.ac.in>
2024-07-09 16:51:20 +0530

    testing pages

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function About() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl">About Page</h1>
+        <p className="text-lg mt-4">This is the about page.</p>
+      </div>
+    );
+  }
